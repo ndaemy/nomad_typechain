7d92dacf678ca61c8186735ad90ec8f9c788310a
yuyaebean <yuyaebean@gmail.com>
2019-08-27 00:31:30 +0900

    Test class in TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-const name = 'Soultree',
-  age = 23,
-  gender = 'male';
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
 
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are ${gender}!`;
+const st = new Human("Yebin", 23, "male");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are ${
+    person.gender
+  }!`;
 };
 
-console.log(sayHi(name, age, gender));
+console.log(sayHi(st));
 
 export {};
